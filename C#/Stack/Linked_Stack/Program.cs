d20d5359bda10a505f09ed2379d94ade63470cd6
MahmoudNofaal <ma7nemahmoud73@gmail.com>
2024-04-13 20:36:50 +0200

    C# [array, linked] stack

---
+++ C#/Stack/Linked_Stack/Program.cs
@@ -0,0 +1,41 @@
+﻿
+/*
+Author: Nofaal
+  Main:
+lookk likke Spider>>>
+*/
+
+
+namespace Nofaal;
+
+
+public class Program
+{
+  // ====| for functions |====
+
+  static void Main(string[] args)
+  {
+    // ===| Code Starts Here |=====
+
+    linkedStack<int> s = new linkedStack<int>();
+    s.push(50);
+    s.push(60);
+    s.push(70);
+    s.push(80);
+    int y;
+    Console.WriteLine("Poped item: " + s.pop(out y));
+    int yy;
+    Console.WriteLine("Top item: " + s.getTop(out yy));
+    Console.WriteLine("Stack elements:");
+    s.Display();
+
+
+    // =======| Code ended |=======
+  }
+}
+
+
+
+
+// |for spilt input|
+//int[] a = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray(); 
---
+++ C#/Stack/Linked_Stack/linkedStack.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nofaal;
+
+public class linkedStack<T>
+{
+  private class Node
+  {
+    public T item;
+    public Node next;
+  }
+
+  private Node stackTop;
+
+  public linkedStack()
+  {
+    stackTop = null;
+  }
+
+
+  public void push(T newItem)
+  {
+    Node newNode = new Node();
+    newNode.item = newItem;
+    newNode.next = stackTop;
+    stackTop = newNode;
+  }
+
+  public bool isEmpty()
+  { return stackTop == null; }
+
+  public void pop()
+  {
+    if(isEmpty())
+    {
+      Console.WriteLine("Stack empty on pop");
+      return;
+    }
+    else
+    {
+      Node temp = stackTop;
+      stackTop = stackTop.next;
+      temp.next = null;
+    }
+  }
+
+  public T pop(out T popItem)
+  {
+    if (isEmpty())
+    {
+      Console.WriteLine("Stack empty on pop");
+      popItem = default(T);
+      return popItem;
+    }
+    else
+    {
+      popItem = stackTop.item;
+      Node temp = stackTop;
+      stackTop = stackTop.next;
+      temp.next = null;
+      return popItem;
+    }
+  }
+
+
+  public T getTop(out T topItem)
+  {
+    if (isEmpty())
+    {
+      Console.WriteLine("Stack empty on getTop");
+      topItem = default(T);
+      return topItem;
+    }
+    else
+    {
+      topItem = stackTop.item;
+      return topItem;
+    }
+  }
+
+
+  public void Display()
+  {
+    Node stackCurr = stackTop;
+    Console.Write("[ ");
+    while(stackCurr!=null)
+    {
+      Console.Write(stackCurr.item + " ");
+      stackCurr = stackCurr.next;
+    }
+    Console.WriteLine("]");
+  }
+}
