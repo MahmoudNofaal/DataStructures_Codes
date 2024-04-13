87b0b97a883476f907099ecb464a5f20f672609a
MahmoudNofaal <ma7nemahmoud73@gmail.com>
2024-04-13 21:38:36 +0200

    C# arraylist

---
+++ C#/Array/ArrayList/ArrayList.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nofaal;
+
+internal class ArrayList
+{
+  private int[] arr;
+  private int maxSize;
+  private int length;
+
+  public ArrayList(int size)
+  {
+    maxSize = Math.Abs(size);
+    arr = new int[maxSize];
+    length = 0;
+  }
+
+  public bool isEmpty()
+  { return length == 0; }
+
+  public bool isFull()
+  { return length == maxSize; }
+
+
+  public void insertAtPos(int pos, int element)
+  {
+    if (isFull())
+    { Console.WriteLine("Array is full"); }
+    else if (pos < 0 || pos > length)
+    { Console.WriteLine("Out of range"); }
+    else
+    {
+      length++;
+      for (int i = length; i <= pos; i++)
+      {
+        arr[i] = arr[i - 1];
+      }
+      arr[pos] = element;
+    }
+  }
+
+
+  public void insertAtEnd(int element)
+  {
+    if (isFull())
+    { Console.WriteLine("Array is full"); }
+    else
+    {
+      arr[length] = element;
+      length++;
+    }
+  }
+
+
+  public int search(int element)
+  {
+    int pos = -1;
+    for (int i = 0; i < length; i++)
+    {
+      if (arr[i] == element)
+      {
+        pos = i;
+        break;
+      }
+    }
+    return pos;
+  }
+
+
+  public void insertNoDuplicate(int element)
+  {
+    if(search(element) == -1)
+    {
+      insertAtEnd(element);
+    }
+    else
+    {
+      Console.WriteLine("Element is already duplicated");
+      return;
+    }
+  }
+
+
+  public void updateAt(int pos, int element)
+  {
+    if(pos < 0 || pos> length)
+    { Console.WriteLine("Out of range"); }
+    else
+    {
+      arr[pos] = element;
+    }
+  }
+
+  public void removeAtPos(int pos)
+  {
+    if (isEmpty())
+    { Console.WriteLine("Array is empty"); }
+    else if (pos < 0 || pos >= length)
+    { Console.WriteLine("Out of range"); }
+    else
+    {
+      for(int i=pos; i<length; i++)
+      {
+        arr[i] = arr[i + 1];
+      }
+      length--;
+    }
+  }
+
+
+  public void removeElement(int element)
+  {
+    int pos = search(element);
+    if (pos != -1)
+    {
+      removeAtPos(pos);
+    }
+    else
+    {
+      Console.WriteLine("Element not found");
+    }
+  }
+
+
+  public int getElement(int pos)
+  {
+    if (isEmpty())
+    {
+      Console.WriteLine("Array is empty");
+      return -1;
+    }
+    else if (pos < 0 || pos >= length)
+    {
+      Console.WriteLine("Out of range");
+      return -1;
+    }
+    else
+    {
+      return arr[pos];
+    }
+  }
+
+
+  public int getSize()
+  {
+    return length;
+  }
+
+
+  public void Display()
+  {
+    Console.Write("Array items: [ ");
+    for (int i = 0; i < length; i++)
+    {
+      Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine("]");
+  }
+
+
+  public void Clear()
+  {
+    length = 0;
+  }
+}
+
---
+++ C#/Array/ArrayList/Program.cs
@@ -0,0 +1,45 @@
+﻿
+/*
+Author: Nofaal
+  Main:
+lookk likke Spider>>>
+*/
+
+
+using System.Collections;
+
+namespace Nofaal;
+
+
+public class Program
+{
+  // ====| for functions |====
+
+  static void Main(string[] args)
+  {
+    // ===| Code Starts Here |=====
+
+    ArrayList arr = new ArrayList(100);
+    arr.insertAtPos(0, 50);
+    arr.insertAtPos(1, 60);
+    arr.insertAtPos(2, 70);
+    arr.insertAtPos(3, 80);
+    arr.insertAtPos(4, 90);
+    arr.insertAtEnd(59);
+    arr.insertNoDuplicate(50);
+    arr.updateAt(3, 88);
+    Console.WriteLine("Element: " + arr.getElement(4));
+    arr.removeAtPos(0);
+    arr.removeElement(90);
+    arr.Display();
+
+
+    // =======| Code ended |=======
+  }
+}
+
+
+
+
+// |for spilt input|
+//int[] a = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray(); 
